b99b02d76dc5964791e1b5dfd8a5f311b72cb268
congtou5 <1160247418@qq.com>
2015-12-30 22:17:53 +0800

    Update config.py
    hide password

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_USERNAME = 'uotgnoc5@163.com'
-    MAIL_PASSWORD = 'ieonatbcoytrqyut'
+    MAIL_PASSWORD = 'xxxxxx'
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <uotgnoc5@163.com>'
     # FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
