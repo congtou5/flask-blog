b43c1c9c6906af8967b6576c6001ac4e413647eb
congtou5 <congtou15@gmail.com>
2015-12-30 22:15:39 +0800

    git whole project

--- config.py
+++ config.py
@@ -18,6 +18,9 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flasky Admin <uotgnoc5@163.com>'
     # FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_ADMIN = 'uotgnoc5@163.com'
+    FLASKY_POSTS_PER_PAGE = 20
+    FLASKY_FOLLOWERS_PER_PAGE = 50
+    FLASKY_COMMENTS_PER_PAGE = 30
 
     @staticmethod
     def init_app(app):
